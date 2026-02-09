776e1397371dde1c3c7df529ccfbeb766ab8aafb
Jeanca296 <jeankmemo@gmail.com>
2026-02-09 15:34:58 -0500

    agrego mi codigo al repositorio

--- main.py
+++ main.py
@@ -1,5 +1,64 @@
-def main():
-  print("Hello learners!")
+def addmultiplenumbers(num1, num2, num3):
+    response = num1 + num2 + num3
+    return response
 
-if __name__=="__main__":
-  main()
+def multiplymultiplenumbers(num1, num2, num3):
+    response = num1 * num2 * num3
+    return response
+
+def isitaninteger(num):
+    response = isinstance(num, int)
+    return response
+
+def isiteven(num):
+    if isitaninteger(num):
+        response = num % 2 == 0
+    else:
+        response = False
+    return response
+
+def addmultiplenumbers(num1, num2, num3):
+    response = num1 + num2 + num3
+    return response
+
+
+def multiplymultiplenumbers(num1, num2, num3):
+    response = num1 * num2 * num3
+    return response
+
+
+def isitaninteger(num):
+    response = isinstance(num, int)
+    return response
+
+
+def isiteven(num):
+    if isitaninteger(num):
+        response = num % 2 == 0
+    else:
+        response = False
+    return response
+
+
+def main():
+    print("Hello learners!")
+
+    num1 = 2
+    num2 = 3
+    num3 = 4
+
+    print("Suma:", addmultiplenumbers(num1, num2, num3))
+    print("Multiplicación:", multiplymultiplenumbers(num1, num2, num3))
+    print("¿Es entero?", isitaninteger(num3))
+    print("¿Es par?", isiteven(num3))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
